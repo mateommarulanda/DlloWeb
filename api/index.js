a947bcfdced40bfd23c0f3687f60fe5c6b330110
Mateo Martinez Marulanda <mateomm01@gmail.com>
2021-09-05 18:52:20 -0500

    categories ready

--- api/index.js
+++ api/index.js
@@ -6,12 +6,16 @@ const app = express()
 
 app.use(express.json())
 
-// IMPORTAR RUTAS
+// IMPORTAR RUTAS USUARIOS
 const router_usuarios = require('./routers/usuarios.router')
 app.use(router_usuarios)
+// IMPORTAR RUTAS CATEGORIAS
+const router_categorias =  require('./routers/categorias.router');
+app.use(router_categorias)
 
 // Definir un puerto, donde se ejecuta el api
-const port = 3000
+const port = 3000 //USUARIOS
+const port = 3001 // CATEGORIAS
 
 // Iniciar el api
 app.listen(port, () => {
