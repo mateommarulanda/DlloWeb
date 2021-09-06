844b3cb46f285525717deb372e28a30d41934838
Mateo Martinez Marulanda <mateomm01@gmail.com>
2021-09-05 22:31:21 -0500

    Reservas and Projects ready

--- api/index.js
+++ api/index.js
@@ -12,10 +12,19 @@ app.use(router_usuarios)
 // IMPORTAR RUTAS CATEGORIAS
 const router_categorias =  require('./routers/categorias.router');
 app.use(router_categorias)
+// IMPORTAR RUTAS PROYECTOS
+const router_proyecto = require('./routers/proyectos.router');
+app.use(router_proyecto)
+// IMPORTAR RUTAS RESERVAS
+const router_reservas = require('./routers/reservas.router');
+app.use(router_reservas)
 
 // Definir un puerto, donde se ejecuta el api
-const port = 3000 //USUARIOS
+const port = 3000 // USUARIOS
 const port = 3001 // CATEGORIAS
+const port = 4000 // PROYECTOS
+const port = 3002 // Reservas
+
 
 // Iniciar el api
 app.listen(port, () => {
